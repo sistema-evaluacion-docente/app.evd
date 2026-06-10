4dbbc6e54a236579c3e679ba55ced94dc56049a0
Andres Parra <byandrev@gmail.com>
2026-06-10 14:52:38 -0500

    feat: integrate TanStack React Query

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,15 +1,27 @@
 import { Toaster } from "@/components/ui/sonner.tsx";
 import { UserProvider } from "@/context/UserContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/index.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <UserProvider>
-      <App />
-      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster />
+      </QueryClientProvider>
     </UserProvider>
   </StrictMode>,
 );
