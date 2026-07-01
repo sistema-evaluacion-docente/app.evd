bdd54ea4389d183a3696ad73e50c9c7b90c4a7a8
Andres Parra <byandrev@gmail.com>
2026-07-01 10:03:27 -0500

    feat: add Figtree variable font and update styles in the application

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,11 @@
 import { Toaster } from "@/components/ui/sonner.tsx";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { UserProvider } from "@/context/UserContext.tsx";
+import "@fontsource-variable/figtree/wght.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import "./styles/index.css";
 
 const queryClient = new QueryClient({
