209a920b3c84f8497247cb27beb69b0cd9abc99c
AlessandroDani <alessandrodaniele07@gmail.com>
2026-07-12 22:28:45 -0500

    feat: add index file for plans feature to export components and hooks

---
+++ src/features/plans/index.ts
@@ -0,0 +1,21 @@
+export { CreatePlanModal } from "./components/CreatePlanModal";
+export { PlanDetailModal } from "./components/PlanDetailModal";
+export { CriticalCasesCard } from "./components/CriticalCasesCard";
+
+export { default as usePlanColumns } from "./hooks/usePlanColumns";
+export { default as useGetPlans } from "./hooks/useGetPlans";
+export { default as useGetPlan } from "./hooks/useGetPlan";
+export { default as useCreatePlan } from "./hooks/useCreatePlan";
+export { default as useUpdatePlan } from "./hooks/useUpdatePlan";
+export { default as useClosePlan } from "./hooks/useClosePlan";
+export { default as useEvaluatePlan } from "./hooks/useEvaluatePlan";
+export { default as useAtRiskTeachers } from "./hooks/useAtRiskTeachers";
+
+export * from "./types/Plan";
+export {
+  PLAN_STATUS_META,
+  PLAN_STATUS_FILTERS,
+  TARGET_TYPE_LABEL,
+  statusMeta,
+  isClosed,
+} from "./lib/planStatus";
