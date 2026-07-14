bee31c5d6f0ec6c010384d668feee23b36af6e15
AlessandroDani <alessandrodaniele07@gmail.com>
2026-07-14 09:03:56 -0500

    refactor: update ThemeSwitcher to use render prop for DropdownMenuTrigger

--- src/components/common/ThemeSwitcher.tsx
+++ src/components/common/ThemeSwitcher.tsx
@@ -14,12 +14,12 @@ export function ThemeSwitcher() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Button variant="outline" size="icon">
-          <Sun className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Cambiar tema</span>
-        </Button>
+      {/* `render` merges the Button into the trigger's own <button>. Passing it
+          as children would nest a second <button> inside the first. */}
+      <DropdownMenuTrigger render={<Button variant="outline" size="icon" />}>
+        <Sun className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span className="sr-only">Cambiar tema</span>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end">
