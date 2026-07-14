771fa627407a7360511a09916e54d7f01c345f3f
AlessandroDani <alessandrodaniele07@gmail.com>
2026-07-14 09:39:17 -0500

    feat: add CreatePlanModal and PlanDetailModal exports for improved plan management

--- src/features/plans/index.ts
+++ src/features/plans/index.ts
@@ -1,6 +1,11 @@
-export { CreatePlanModal } from "./components/CreatePlanModal";
-export { PlanDetailModal } from "./components/PlanDetailModal";
 export { CriticalCasesCard } from "./components/CriticalCasesCard";
+export { ItemEditor } from "./components/ItemEditor";
+export { WeaknessHints } from "./components/WeaknessHints";
+export { ActaSection } from "./components/ActaSection";
+export { EvidencesSection } from "./components/EvidencesSection";
+export { PlanDetailBody, StatusBadge } from "./components/PlanDetailBody";
+export { TeacherPlanHistorySection } from "./components/TeacherPlanHistorySection";
+export { default as MyPlanCard } from "./components/MyPlanCard";
 
 export { default as usePlanColumns } from "./hooks/usePlanColumns";
 export { default as useGetPlans } from "./hooks/useGetPlans";
@@ -12,6 +17,13 @@ export { default as useEvaluatePlan } from "./hooks/useEvaluatePlan";
 export { default as useAtRiskTeachers } from "./hooks/useAtRiskTeachers";
 export { default as usePlanCandidates } from "./hooks/usePlanCandidates";
 export { default as usePlanIndicators } from "./hooks/usePlanIndicators";
+export { default as usePlanPeriods } from "./hooks/usePlanPeriods";
+export { default as useUploadActa } from "./hooks/useUploadActa";
+export { useAddEvidence, useDeleteEvidence } from "./hooks/useEvidences";
+export { default as useMyPlans } from "./hooks/useMyPlans";
+export { default as useTeacherPlanHistory } from "./hooks/useTeacherPlanHistory";
+
+export { default as uploadActa } from "./api/uploadActa";
 
 export * from "./types/Plan";
 export {
@@ -22,3 +34,13 @@ export {
   statusMeta,
   isClosed,
 } from "./lib/planStatus";
+export {
+  describe,
+  nextKey,
+  targetKey,
+  OVERALL,
+  QUALITATIVE,
+  WHOLE_DIMENSION,
+  type DraftItem,
+} from "./lib/draft";
+export { uploadedFileUrl } from "./lib/fileUrl";
