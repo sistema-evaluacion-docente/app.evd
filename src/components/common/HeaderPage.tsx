38a92f6241911b9a2242949912df589d7bdaf7e7
Andres Parra <byandrev@gmail.com>
2026-07-16 20:19:57 -0500

    refactor: simplify HeaderPage component by removing unused Breadcrumb and adjusting formatting

--- src/components/common/HeaderPage.tsx
+++ src/components/common/HeaderPage.tsx
@@ -1,45 +1,31 @@
-import { Link } from "wouter";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "../ui/breadcrumb";
-
 type HeaderPageProps = {
-  title: string;
-  children?: React.ReactNode;
-  icon?: React.ReactNode;
-};
+  title: string
+  children?: React.ReactNode
+  icon?: React.ReactNode
+}
 
+/**
+ * HeaderPage component is used to display a header with a title, an optional icon, and optional children elements.
+ *
+ * @param {HeaderPageProps} props - The properties for the HeaderPage component.
+ * @param {string} props.title - The title to be displayed in the header.
+ * @param {React.ReactNode} [props.children] - Optional children elements to be displayed in the header.
+ * @param {React.ReactNode} [props.icon] - Optional icon to be displayed next to the title.
+ * @returns {JSX.Element} The rendered HeaderPage component.
+ */
 function HeaderPage({ title, children, icon }: HeaderPageProps) {
   return (
     <header>
-      <Breadcrumb className="mb-4">
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <Link href="/dashboard">Inicio</Link>
-          </BreadcrumbItem>
-
-          <BreadcrumbSeparator />
-
-          <BreadcrumbItem>
-            <BreadcrumbPage>{title}</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
           {icon}
-          <h2 className="font-bold text-xl">{title}</h2>
+          <h2 className="text-xl font-bold">{title}</h2>
         </div>
 
         {children}
       </div>
     </header>
-  );
+  )
 }
 
-export default HeaderPage;
+export default HeaderPage
