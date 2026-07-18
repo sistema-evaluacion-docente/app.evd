38feb53575e417cb24f5b2fdb2edc37c92b55a2f
Andres Parra <byandrev@gmail.com>
2026-07-18 18:04:08 -0500

    refactor: enhance getFaculties function by improving parameter handling and formatting

--- src/features/faculties/api/getFaculties.ts
+++ src/features/faculties/api/getFaculties.ts
@@ -1,17 +1,31 @@
-import api from "@/config/axios";
-import type { ResponseAPI } from "@/shared/types/Response";
-import type { Faculty } from "../types/Faculty";
+import api from '@/config/axios'
+import type { ResponseAPI } from '@/shared/types/Response'
+import type { Faculty } from '../types/Faculty'
 
+/**
+ * Fetches faculties from the API.
+ *
+ * @param page - The page number to fetch (default is 1).
+ * @param limit - The number of items per page (default is 10).
+ * @param search - Optional search query to filter faculties.
+ * @returns A promise that resolves to the API response containing an array of faculties.
+ */
 export default function getFaculties({
   page = 1,
   limit = 10,
-  search
+  search,
 }: {
-  page?: number,
-  limit?: number,
+  page?: number
+  limit?: number
   search?: string
 }): Promise<ResponseAPI<Faculty[]>> {
-  return api.get("/faculties", {
-    params: { page, limit, search }
-  });
+  const params: Record<string, string | number> = {}
+
+  if (search) {
+    params.search = search
+  }
+
+  return api.get('/faculties', {
+    params: { page, limit, search },
+  })
 }
