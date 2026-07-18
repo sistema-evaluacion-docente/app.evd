2aaea6f3d2bf31f04d2bfa0d85b777f9fc9a6440
Andres Parra <byandrev@gmail.com>
2026-07-18 18:03:27 -0500

    refactor: improve getDepartments function by enhancing parameter handling and formatting

--- src/features/departments/api/getDepartments.ts
+++ src/features/departments/api/getDepartments.ts
@@ -1,17 +1,31 @@
-import api from "@/config/axios";
-import type { ResponseAPI } from "@/shared/types/Response";
-import type { Department } from "../types/Department";
+import api from '@/config/axios'
+import type { ResponseAPI } from '@/shared/types/Response'
+import type { Department } from '../types/Department'
 
+/**
+ * Fetches departments from the API.
+ *
+ * @param page - The page number to fetch (default is 1).
+ * @param limit - The number of items per page (default is 10).
+ * @param search - Optional search query to filter departments.
+ * @returns A promise that resolves to the API response containing an array of departments.
+ */
 export default function getDepartments({
   page = 1,
   limit = 10,
-  search
+  search,
 }: {
-  page?: number,
-  limit?: number,
+  page?: number
+  limit?: number
   search?: string
 }): Promise<ResponseAPI<Department[]>> {
-  return api.get("/departments", {
-    params: { page, limit, search }
-  });
+  const params: Record<string, string | number> = {}
+
+  if (search) {
+    params.search = search
+  }
+
+  return api.get('/departments', {
+    params: { page, limit, ...params },
+  })
 }
