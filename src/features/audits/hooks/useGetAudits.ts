b86ac209c3083f974d33579ea7a8dbdcd1891308
Andres Parra <byandrev@gmail.com>
2026-07-18 19:06:42 -0500

    refactor: enhance code consistency and improve formatting in useGetAudits hook

--- src/features/audits/hooks/useGetAudits.ts
+++ src/features/audits/hooks/useGetAudits.ts
@@ -1,6 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
-import getAudits from "../api/getAudits";
+import { useQuery } from '@tanstack/react-query'
+import getAudits from '../api/getAudits'
 
+/**
+ * Custom hook to fetch audits based on provided parameters.
+ * @param params - An object containing the parameters for fetching audits.
+ * @returns The result of the useQuery hook, including data, error, and status.
+ */
 export default function useGetAudits({
   page,
   limit,
@@ -10,25 +15,16 @@ export default function useGetAudits({
   start_date,
   end_date,
 }: {
-  page: number;
-  limit: number;
-  search: string;
-  table_name?: string;
-  operation?: string;
-  start_date?: string;
-  end_date?: string;
+  page: number
+  limit: number
+  search: string
+  table_name?: string
+  operation?: string
+  start_date?: string
+  end_date?: string
 }) {
   return useQuery({
-    queryKey: [
-      "audits",
-      page,
-      limit,
-      search,
-      table_name,
-      operation,
-      start_date,
-      end_date,
-    ],
+    queryKey: ['audits', page, limit, search, table_name, operation, start_date, end_date],
     queryFn: () =>
       getAudits({
         page,
@@ -39,7 +35,5 @@ export default function useGetAudits({
         start_date,
         end_date,
       }),
-    staleTime: 5 * 60 * 1000,
-    gcTime: 10 * 60 * 1000,
-  });
+  })
 }
