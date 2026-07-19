a58d4f85e00d6fc09b1e72189d5fbab94689bfb3
Andres Parra <byandrev@gmail.com>
2026-07-19 18:33:06 -0500

    refactor: standardize quotes and remove unnecessary console.log in formatDate function

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,13 +1,17 @@
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import localeEs from "dayjs/locale/es";
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import localeEs from 'dayjs/locale/es'
 
-dayjs.extend(utc);
-dayjs.locale(localeEs);
+dayjs.extend(utc)
+dayjs.locale(localeEs)
 
+/**
+ * Formats a date string into a human-readable format.
+ * @param value - The date string to format.
+ * @returns A formatted date string in the format "MMMM D, YYYY h:mm A".
+ */
 function formatDate(value: string) {
-  console.log(value);
-  return dayjs(value).format("MMMM D, YYYY h:mm A");
+  return dayjs(value).format('MMMM D, YYYY h:mm A')
 }
 
-export default formatDate;
+export default formatDate
