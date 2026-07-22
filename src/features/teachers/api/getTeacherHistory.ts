109099587676721f2b9c8f15a914afd59ca5fc22
Andres Parra <byandrev@gmail.com>
2026-07-22 18:08:01 -0500

    refactor: clean up formatting and improve documentation for getTeacherHistory function

--- src/features/teachers/api/getTeacherHistory.ts
+++ src/features/teachers/api/getTeacherHistory.ts
@@ -1,9 +1,13 @@
-import api from "@/config/axios";
-import type { ResponseAPI } from "@/shared/types/Response";
-import type { TeacherHistoryData } from "../types/Teacher";
+import api from '@/config/axios'
+import type { ResponseAPI } from '@/shared/types/Response'
+import type { TeacherHistoryData } from '../types/Teacher'
 
-export default function getTeacherHistory(
-  id: number,
-): Promise<ResponseAPI<TeacherHistoryData>> {
-  return api.get(`/teachers/${id}/history`);
+/**
+ * Fetches the history of a teacher by their ID.
+ *
+ * @param id - The ID of the teacher whose history is to be fetched.
+ * @returns A promise that resolves to the teacher's history data wrapped in a ResponseAPI object.
+ */
+export default function getTeacherHistory(id: number): Promise<ResponseAPI<TeacherHistoryData>> {
+  return api.get(`/teachers/${id}/history`)
 }
