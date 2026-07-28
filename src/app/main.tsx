9efd698de083c89c534b9a43181f81515c0b5e5b
Andres Parra <byandrev@gmail.com>
2026-07-28 11:51:04 -0500

    fix: correct import path for NotificationsProvider in main.tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from '@/components/ui/sonner.tsx'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { UserProvider } from '@/context/UserContext.tsx'
-import { NotificationsProvider } from '@/features/notifications/context/NotificationsContext'
+import { NotificationsProvider } from '@/features/notifications/context/NotificationsContext.tsx'
 import '@fontsource-variable/figtree/wght.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'next-themes'
